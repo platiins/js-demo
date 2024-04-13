c63192c6024f76b9e0e4dd3737ebfff659830819
platiins <valerijaaivanovaa@gmail.com>
2024-04-13 08:19:20 +0300

    adding page with task 3

---
+++ js/square.js
@@ -0,0 +1,5 @@
+const squareSide = prompt("please enter a length of a side of a square:");
+
+let squarePerimeter = squareSide * 4;
+
+document.body.innerHTML = `<h1>The perimeter of the square is ${squarePerimeter}</h1>`;
