f02cab41ff2922a17f00616f1a9658f6fcdda077
platiins <valerijaaivanovaa@gmail.com>
2024-04-13 09:42:13 +0300

    adding page with task7

---
+++ js/questions.js
@@ -0,0 +1,31 @@
+const userName = prompt("your name:");
+
+const userSurname = prompt("your surname:");
+
+const userMiddleName = prompt("your middle name:");
+
+const userAge = prompt("your age:");
+
+const userCountry = prompt("your country:");
+
+const userCity = prompt("your city:");
+
+const userJob = prompt("your job:");
+
+const userHobby = prompt("your hobby:");
+
+const userColor = prompt("your favourite color:");
+
+const userMood = prompt("your mood:");
+
+alert(`please confirm answers:
+1.${userName}
+2.${userSurname}
+3. ${userMiddleName}
+4. ${userAge}
+5. ${userCountry}
+6. ${userCity}
+7. ${userJob}
+8. ${userHobby}
+9. ${userColor}
+10. ${userMood}`);
