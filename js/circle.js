5fb6b7c362bb1694e4490ded8508663fac664c81
platiins <valerijaaivanovaa@gmail.com>
2024-04-13 08:29:17 +0300

    adding page with task 4

---
+++ js/circle.js
@@ -0,0 +1,7 @@
+const circleRadius = prompt("please enter a radius of a circle:");
+
+const pi = 3.14;
+
+let circleArea = circleRadius ** 2 * pi;
+
+document.body.innerHTML = `<h1>The area of the circle is ${circleArea}</h1>`;
