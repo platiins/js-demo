66f64f5fd17ff6beceb21f2a7d060f6327012308
platiins <valerijaaivanovaa@gmail.com>
2024-04-13 10:31:38 +0300

    adjusting page 7 and creating page with task 8

--- js/about.js
+++ js/about.js
@@ -7,25 +7,6 @@ const userAge = prompt("please enter your age:");
 
 const userCountry = prompt("please enter your country of birth:");
 
-console.log(`info about you:
-
-your name: ${userName}, 
-your age: ${userAge},
-your country of birth: ${userCountry}`);
-
-// to display on screen
-
-/* 
-
-document.body.innerHTML = `<h1>info about you:</h1>
-<ul>
-    <li class="name">your name: ${userName}</li>
-    <li class="age">your age: ${userAge}</li>
-    <li class="country">your country of birth: ${userCountry}</li>
-</ul>`; 
-
-document.querySelector(".name").style.color = "red";
-document.querySelector(".age").style.color = "green";
-document.querySelector(".country").style.color = "blue";
-
-*/
+console.log(`%cyour name: ${userName}`, "color:green");
+console.log(`%cyour age: ${userAge}`, "color:lightblue");
+console.log(`%cyour country: ${userCountry}`, "color:red");
---
+++ js/future-age.js
@@ -0,0 +1,7 @@
+const birthYear = 2001;
+
+console.log(`(2025 - ${birthYear})
+2029 - ${birthYear}
+2050 - ${birthYear}`);
+
+
